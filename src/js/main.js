373591e11e64a0a88c762ede04e34e4a293bb849
Soveliy <nikita.elagin2012@yandex.ru>
2023-06-26 17:35:53 +0300

    added rocket animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+let bg = document.querySelector('.hero__right img');
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+});
